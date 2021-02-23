b3ae1a609f4f733a09180d36f62a60827e02bc6b
meilihuang <c14180011@john.petra.ac.id>
2021-02-23 17:25:00 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+
+  item = [
+    {nama: "Jeruk", harga: 15000, satuan: "kg"},
+    {nama: "Apel", harga: 5000, satuan: "kg"}
+  ]
+
+  tampil : boolean = false;
+  tulisanOPEN : String; 
+
+  OPEN(){
+    this.tulisanOPEN = "TOKO DIBUKA";
+    alert("DIBUKA");
+  }
+
+  value = "";
+  onKey(event: any){
+    this.value+= event.target.value+"|";
+  }
 }
